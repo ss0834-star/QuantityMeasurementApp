ccea8f8c6a826f3e4994d16f23d48c1706c19892
ss0834-star <ss0834@srmist.edu.in>
2026-05-06 09:34:49 +0530

    UC2: Feet and Inches measurement equality

--- src/main/java/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp.java
@@ -4,6 +4,9 @@ public class QuantityMeasurementApp {
         private final double value;
 
         public Feet(double value) {
+            if (!Double.isFinite(value)) {
+                throw new IllegalArgumentException("Feet value must be numeric");
+            }
             this.value = value;
         }
 
@@ -22,10 +25,54 @@ public class QuantityMeasurementApp {
         }
     }
 
-    public static void main(String[] args) {
-        Feet value1 = new Feet(1.0);
-        Feet value2 = new Feet(1.0);
+    static class Inches {
+        private final double value;
 
-        System.out.println(value1.equals(value2));
+        public Inches(double value) {
+            if (!Double.isFinite(value)) {
+                throw new IllegalArgumentException("Inches value must be numeric");
+            }
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            Inches inches = (Inches) obj;
+            return Double.compare(inches.value, value) == 0;
+        }
+    }
+
+    static boolean compareFeetEquality(double firstValue, double secondValue) {
+        Feet firstFeet = new Feet(firstValue);
+        Feet secondFeet = new Feet(secondValue);
+        return firstFeet.equals(secondFeet);
+    }
+
+    static boolean compareInchesEquality(double firstValue, double secondValue) {
+        Inches firstInches = new Inches(firstValue);
+        Inches secondInches = new Inches(secondValue);
+        return firstInches.equals(secondInches);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Output: Equal (" + compareFeetEquality(1.0, 1.0) + ")");
+
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + compareInchesEquality(1.0, 1.0) + ")");
+
+        System.out.println("Input: 1.0 ft and 2.0 ft");
+        System.out.println("Output: Equal (" + compareFeetEquality(1.0, 2.0) + ")");
+
+        System.out.println("Input: 1.0 inch and 2.0 inch");
+        System.out.println("Output: Equal (" + compareInchesEquality(1.0, 2.0) + ")");
     }
 }
