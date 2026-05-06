88b99623e139b4fa2fa8ca920e05312ed79ed9b3
ss0834-star <ss0834@srmist.edu.in>
2026-05-06 09:47:48 +0530

    UC6: Add length quantities

--- src/main/java/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp.java
@@ -18,13 +18,14 @@ public class QuantityMeasurementApp {
     }
 
     static class QuantityLength {
+        private static final double EPSILON = 0.000001;
 
         private final double value;
         private final LengthUnit unit;
 
         public QuantityLength(double value, LengthUnit unit) {
             if (!Double.isFinite(value)) {
-                throw new IllegalArgumentException("Value must be numeric");
+                throw new IllegalArgumentException("Invalid numeric value");
             }
 
             if (unit == null) {
@@ -39,6 +40,52 @@ public class QuantityMeasurementApp {
             return value * unit.getConversionFactor();
         }
 
+        public QuantityLength convertTo(LengthUnit targetUnit) {
+            if (targetUnit == null) {
+                throw new IllegalArgumentException("Target unit cannot be null");
+            }
+
+            double valueInFeet = convertToFeet();
+            double convertedValue = valueInFeet / targetUnit.getConversionFactor();
+
+            return new QuantityLength(convertedValue, targetUnit);
+        }
+
+        public static double convert(double value, LengthUnit sourceUnit, LengthUnit targetUnit) {
+            if (!Double.isFinite(value)) {
+                throw new IllegalArgumentException("Invalid numeric value");
+            }
+
+            if (sourceUnit == null || targetUnit == null) {
+                throw new IllegalArgumentException("Units cannot be null");
+            }
+
+            double valueInFeet = value * sourceUnit.getConversionFactor();
+
+            return valueInFeet / targetUnit.getConversionFactor();
+        }
+
+        public QuantityLength add(QuantityLength other) {
+            if (other == null) {
+                throw new IllegalArgumentException("Other quantity cannot be null");
+            }
+
+            double thisValueInFeet = this.convertToFeet();
+            double otherValueInFeet = other.convertToFeet();
+            double sumInFeet = thisValueInFeet + otherValueInFeet;
+            double resultValue = sumInFeet / this.unit.getConversionFactor();
+
+            return new QuantityLength(resultValue, this.unit);
+        }
+
+        public static QuantityLength add(QuantityLength first, QuantityLength second) {
+            if (first == null || second == null) {
+                throw new IllegalArgumentException("Quantities cannot be null");
+            }
+
+            return first.add(second);
+        }
+
         @Override
         public boolean equals(Object obj) {
             if (this == obj) {
@@ -51,21 +98,31 @@ public class QuantityMeasurementApp {
 
             QuantityLength other = (QuantityLength) obj;
 
-            return Math.abs(this.convertToFeet() - other.convertToFeet()) < 0.000001;
+            return Math.abs(this.convertToFeet() - other.convertToFeet()) < EPSILON;
+        }
+
+        @Override
+        public String toString() {
+            return value + " " + unit;
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(new QuantityLength(1.0, LengthUnit.YARDS)
-                .equals(new QuantityLength(3.0, LengthUnit.FEET)));
+        System.out.println(QuantityLength.convert(1.0, LengthUnit.FEET, LengthUnit.INCHES));
+        System.out.println(QuantityLength.convert(3.0, LengthUnit.YARDS, LengthUnit.FEET));
+        System.out.println(QuantityLength.convert(36.0, LengthUnit.INCHES, LengthUnit.YARDS));
+        System.out.println(QuantityLength.convert(1.0, LengthUnit.CENTIMETERS, LengthUnit.INCHES));
+
+        System.out.println(new QuantityLength(1.0, LengthUnit.FEET)
+                .add(new QuantityLength(12.0, LengthUnit.INCHES)));
+
+        System.out.println(new QuantityLength(12.0, LengthUnit.INCHES)
+                .add(new QuantityLength(1.0, LengthUnit.FEET)));
 
         System.out.println(new QuantityLength(1.0, LengthUnit.YARDS)
-                .equals(new QuantityLength(36.0, LengthUnit.INCHES)));
+                .add(new QuantityLength(3.0, LengthUnit.FEET)));
 
-        System.out.println(new QuantityLength(2.0, LengthUnit.CENTIMETERS)
-                .equals(new QuantityLength(2.0, LengthUnit.CENTIMETERS)));
-
-        System.out.println(new QuantityLength(1.0, LengthUnit.CENTIMETERS)
-                .equals(new QuantityLength(0.393701, LengthUnit.INCHES)));
+        System.out.println(new QuantityLength(5.0, LengthUnit.FEET)
+                .add(new QuantityLength(-2.0, LengthUnit.FEET)));
     }
 }
